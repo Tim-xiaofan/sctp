1ba0f5f2fa6c13ac9573642b110eda04f65ff1be
zyjie <1305696765@qq.com>
2020-08-14 16:51:18 +0800

    office

--- 00test/client_q_date.c
+++ 00test/client_q_date.c
@@ -14,100 +14,43 @@
 #define GMT_STREAM 256
 int main()
 {
-	int connSock, in, i, ret, flags;
+	int connSock, in, i, flags;
 	struct sockaddr_in servaddr;
-	struct sctp_status status;
-	struct sctp_sndrcvinfo sndrcvinfo;//sctp_sndrcvinfo结构体，收到消息的时候会填写这个结构体，是一个输出参数
+	struct sctp_sndrcvinfo sndrcvinfo;
 	struct sctp_event_subscribe events;
-	struct sctp_initmsg initmsg;//获取或设置某个SCTP套接字在发送INIT消息时所用的默认初始参数
 	char buffer[MAX_BUFFER + 1];
-
 	/* Create an SCTP TCP-Style Socket */
-	/*创建套接字,协议族AP_INET:IP，socket类型SOCKET:有序的、可靠的，IPPROTO_SCTP:指定为SCTP协议*/
 	connSock = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP);
-	if(connSock == -1)
-	{
-		printf("Socket ");
-		exit(errno);
-	}
-	/* Specify that a maximum of 5 streams will be available per socket */
-	memset(&initmsg, 0, sizeof(initmsg));
-	initmsg.sinit_num_ostreams = 5;//应用进程想要请求的外出sctp流的数目
-	initmsg.sinit_max_instreams = 5;//应用进程准备允许的外来SCTP流的最大数目
-	initmsg.sinit_max_attempts = 4;//SCTP协议栈应该重传多少次初始INIT消息才认为对端不可达
-	//设置套接字
-	ret = setsockopt(connSock, IPPROTO_SCTP, SCTP_INITMSG,
-					 &initmsg, sizeof(initmsg));
-
 	/* Specify the peer endpoint to which we'll connect */
 	bzero((void *)&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(MY_PORT_NUM);
 	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-	/* Connect to the server 建立连接*/
-	ret = connect(connSock, (struct sockaddr *)&servaddr, sizeof(servaddr));
-	if(ret == -1)
-	{
-		printf("connect ");
-		exit(errno);
-	}
-
+	/* Connect to the server */
+	connect(connSock, (struct sockaddr *)&servaddr, sizeof(servaddr));
 	/* Enable receipt of SCTP Snd/Rcv Data via sctp_recvmsg */
-	events.sctp_data_io_event = 1;
 	memset((void *)&events, 0, sizeof(events));
 	events.sctp_data_io_event = 1;
-	ret = setsockopt(connSock, SOL_SCTP, SCTP_EVENTS,
-					 (const void *)&events, sizeof(events));//事件注册
-	if(ret == -1)
-	{
-		printf("setsockopt event");
-		exit(errno);
-	}
-
-	/* Read and emit the status of the Socket (optional step) */
-	in = sizeof(status);
-	ret = getsockopt(connSock, SOL_SCTP, SCTP_STATUS,
-					 (void *)&status, (socklen_t *)&in);
-	if(ret == -1)
-	{
-		printf("setsockopt ");
-		exit(errno);
-	}
-
-	/*printf("assoc id = %d\n", status.sstat_assoc_id);
-	printf("state = %d\n", status.sstat_state);
-	printf("instrms = %d\n", status.sstat_instrms);
-	printf("outstrms = %d\n", status.sstat_outstrms);*/
-
+	setsockopt(connSock, SOL_SCTP, SCTP_EVENTS,
+			   (const void *)&events, sizeof(events));
 	/* Expect two messages from the peer */
-
 	for (i = 0; i < 2; i++)
 	{
-		//从connSock参数所指定的 SCTP 端点接收消息, buff是消息内容
 		in = sctp_recvmsg(connSock, (void *)buffer, sizeof(buffer),
-						  (struct sockaddr *)NULL, 0, &sndrcvinfo, &flags);
-		if(in < 0){
-			printf("sctp_revmsg ");
-			exit(errno);
-		}
-		if (in > 0)
+						  (struct sockaddr *)NULL, 0,
+						  &sndrcvinfo, &flags);
+		/* Null terminate the incoming string */
+		buffer[in] = 0;
+		if (sndrcvinfo.sinfo_stream == LOCALTIME_STREAM)
 		{
-			buffer[in] = 0;
 			printf("(Local) %s\n", buffer);
-			/*if (sndrcvinfo.sinfo_stream == LOCALTIME_STREAM)
-			{
-				printf("(Local) %s\n", buffer);
-			}
-			else if (sndrcvinfo.sinfo_stream == GMT_STREAM)
-			{
-				printf("(GMT ) %s\n", buffer);
-			}*/
+		}
+		else if (sndrcvinfo.sinfo_stream == GMT_STREAM)
+		{
+			printf("(GMT  ) %s\n", buffer);
 		}
 	}
-
 	/* Close our socket and exit */
 	close(connSock);
-
 	return 0;
 }
